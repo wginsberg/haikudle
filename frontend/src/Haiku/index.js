1a9f093bb313e456f00f1970a10d458d68f67fca
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-06-15 08:59:49 -0500

    fix haiku bottom margin

--- frontend/src/Haiku/index.js
+++ frontend/src/Haiku/index.js
@@ -12,27 +12,24 @@ export default function Haiku({ haiku, haikuCensored, input }) {
     return (
         <div className="haiku">
             {censoredWordArray2d.map((line, i) => (
-                <div key={i}>
-                    <div className="line">
-                        {line.map((word, j) => {
-                            const isComplete = word === wordArray2d[i][j]
-                            return (
-                                <span className="word" key={j}>
-                                    {word
-                                        .split("")
-                                        .map((character, k) => (
-                                            <Tile
-                                                character={character}
-                                                wordIsComplete={isComplete}
-                                                meta={metaArray2d[i][j][k]}
-                                                key={`${i}-${j}-${k}`}
-                                            />
-                                        ))}
-                                </span>
-                            )
-                        })}
-                    </div>
-                    <br />
+                <div className="line" key={i}>
+                    {line.map((word, j) => {
+                        const isComplete = word === wordArray2d[i][j]
+                        return (
+                            <span className="word" key={j}>
+                                {word
+                                    .split("")
+                                    .map((character, k) => (
+                                        <Tile
+                                            character={character}
+                                            wordIsComplete={isComplete}
+                                            meta={metaArray2d[i][j][k]}
+                                            key={`${i}-${j}-${k}`}
+                                        />
+                                    ))}
+                            </span>
+                        )
+                    })}
                 </div>
             ))}
         </div>
