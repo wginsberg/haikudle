01c7ea508048b3722fc8548ee3857b5747c47993
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-09-23 12:20:20 -0400

    new colour behaviour

--- frontend/src/Haiku/index.js
+++ frontend/src/Haiku/index.js
@@ -1,3 +1,4 @@
+import classnames from 'classnames'
 import Tile from './Tile'
 import { addInputToHaiku, charactersTo2DStringArray } from '../util'
 
@@ -8,31 +9,39 @@ export default function Haiku ({ haiku, haikuCensored, input }) {
   const censoredWordArray2d = charactersTo2DStringArray(characters)
   const metaArray2d = charactersTo2DStringArray(meta)
 
+  const currentLine = metaArray2d.findIndex(line => line.find(word => word.includes('*')))
+  const activeLine = currentLine === -1 ? 2 : currentLine
+
   return (
     <div className='haiku'>
-      {censoredWordArray2d.map((line, i) => (
-        <div className='line' key={i}>
-          {line.map((word, j) => {
-            const isComplete = word === wordArray2d[i][j]
-            const wordIsIncorrect = !isComplete && !word.includes('*')
-            return (
-              <span className='word' key={j}>
-                {word
-                  .split('')
-                  .map((character, k) => (
-                    <Tile
-                      character={character}
-                      wordIsIncorrect={wordIsIncorrect}
-                      wordIsComplete={isComplete}
-                      meta={metaArray2d[i][j][k]}
-                      key={`${i}-${j}-${k}`}
-                    />
-                  ))}
-              </span>
-            )
-          })}
-        </div>
-      ))}
+      {censoredWordArray2d.map((line, i) => {
+        const isActiveLine = i <= activeLine
+        const className = classnames('line', { 'line-inactive': !isActiveLine })
+        return (
+          <div className={className} key={i}>
+            {line.map((word, j) => {
+              const isComplete = word === wordArray2d[i][j]
+              const wordIsIncorrect = !isComplete && !word.includes('*')
+              const displayIncorrectWord = wordIsIncorrect && i < activeLine
+              return (
+                <span className='word' key={j}>
+                  {word
+                    .split('')
+                    .map((character, k) => (
+                      <Tile
+                        character={character}
+                        wordIsIncorrect={displayIncorrectWord}
+                        wordIsComplete={isComplete}
+                        meta={metaArray2d[i][j][k]}
+                        key={`${i}-${j}-${k}`}
+                      />
+                    ))}
+                </span>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }
